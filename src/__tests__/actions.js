be9e18111101d06b2bcfc6c25724a2814ff3d904
brt <dubudabu@gmail.com>
2023-01-04 21:59:25 +0100

    W11L10 - homework: naive Redux testing

--- src/__tests__/actions.js
+++ src/__tests__/actions.js
@@ -1,4 +1,10 @@
-import { setTimeboxes } from "../components/actions";
+import { setTimeboxes, replaceTimebox } from "../components/actions";
 test("setTimeboxes emits TIMEBOXES_SET action", () => {
   expect(setTimeboxes([])).toEqual({ type: "TIMEBOXES_SET", timeboxes: [] });
+});
+test("replaceTimebox emits TIMEBOX_REPLACE action", () => {
+  expect(replaceTimebox({ id: 1 })).toEqual({
+    type: "TIMEBOX_REPLACE",
+    replacedTimebox: { id: 1 },
+  });
 });
--- src/__tests__/selectors.js
+++ src/__tests__/selectors.js
@@ -1,8 +1,15 @@
-import { areTimeboxesLoading } from "../components/selectors";
+import { areTimeboxesLoading, getTimeboxById } from "../components/selectors";
 
 test("areTimeboxesLoading returns true when state.areTimeboxesLoading is set to true", () => {
   const state = {
     timeboxesAreLoading: true,
   };
   expect(areTimeboxesLoading(state)).toBe(true);
+});
+
+test("getTimeboxById returns timebox when given its Id", () => {
+  const state = {
+    timeboxes: [{ id: 1 }],
+  };
+  expect(getTimeboxById(state, 1)).toEqual({ id: 1 });
 });
